3dea9f923a1aa82c7f9c1b77fb558fdd474111ac
tsubasan1122 <tsubasan1122@gmail.com>
2019-01-24 10:39:20 +0000

    #2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i(index show)
 end
